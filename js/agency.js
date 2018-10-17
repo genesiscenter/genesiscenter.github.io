82b65a96bb6da4e380edf63b435c0747db7bbdee
Adam Jonas <jonas@flatironschool.com>
2018-10-16 22:03:47 -0400

    expand out FAQ for the night

--- js/agency.js
+++ js/agency.js
@@ -26,6 +26,18 @@
     offset: 56
   });
 
+
+  // var mql = window.matchMedia('(max-width: 600px)');
+  //
+  // function screenTest(e) {
+  //   if (e.matches) {
+  //     /* the viewport is 600 pixels wide or less */
+  //     $(".card .collapse").addClass('show')
+  //   }
+  // }
+  //
+  // mql.addListener(screenTest);
+
   // Collapse Navbar
   // var navbarCollapse = function() {
   //   if ($("#mainNav").offset().top > 100) {
@@ -34,10 +46,10 @@
   //     $("#mainNav").removeClass("navbar-shrink");
   //   }
   // };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  // // Collapse now if page is not at top
+  // navbarCollapse();
+  // // Collapse the navbar when page is scrolled
+  // $(window).scroll(navbarCollapse);
 
   // Hide navbar when modals trigger
   $('.portfolio-modal').on('show.bs.modal', function(e) {
