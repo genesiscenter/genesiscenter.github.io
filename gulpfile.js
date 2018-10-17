087db0d94c551590705f07e6366693ac334fe4a9
Adam Jonas <jonas@flatironschool.com>
2018-10-17 12:45:45 -0400

    more gulp build updates

--- gulpfile.js
+++ gulpfile.js
@@ -122,17 +122,8 @@ gulp.task('dev', ['css', 'js', 'browserSync'], function() {
 });
 
 // Gulp Build
-var build = require('gulp-build');
-
-var options = {
-  helpers: [{
-    name: 'addition',
-    fn: function(a, b) { return a + b; }
-  }]
-};
-
-gulp.task('build', function() {
-  gulp.src('pages/*.html')
-      .pipe(build({ title: 'Some page' }, options))
-      .pipe(gulp.dest('dist'))
+gulp.task("build", ['css:compile', 'css:minify', 'js:minify'], function() {
+   return gulp.src(['css/agency.min.css', 'js/agency.min.js', 'index.html',
+                    'img/**', 'vendor/**'], { base: './'})
+       .pipe(gulp.dest('dist'));
 });
